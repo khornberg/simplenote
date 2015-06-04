a9426a6da3fc7893480c3f223374e05e170654ec
Kyle Hornberg <kyle.hornberg@gmail.com>
2015-06-04 17:23:08 -0500

    Store token in class. Does not time out.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ class SimpleNote {
   constructor(email, password) {
     this.email = email;
     this.password = password;
+    this.token = null;
     this.notes = [];
     this.api = "https://simple-note.appspot.com/api/";
     this.api2 = "https://simple-note.appspot.com/api2/";
@@ -20,73 +21,83 @@ class SimpleNote {
   auth() {
     // base64 encoding of auth params
     var query = encode("email=" + this.email + '&password=' + this.password),
-        self = this;
+      self = this;
 
-    return new Promise (
-      function (resolve, reject) {
-        request
-          .post(self.api + 'login')
-          .send(query)
-          .end(function(err, res) {
-            if(res.error) {
-              reject(res.error);
-            }
-            resolve(res.text);
-          });
-      });
+    if (self.token === null) {
+      return new Promise(
+        function(resolve, reject) {
+          request
+            .post(self.api + 'login')
+            .send(query)
+            .end(function(err, res) {
+              if (res.error) {
+                reject(res.error);
+              }
+              self.token = res.text;
+              resolve(res.text);
+            });
+        });
+    }
+
+    return Promise.resolve(self.token);
   }
 
   all(len) {
-    if(arguments.length == 1) fn = len;
-
     // 100 is the highest value you can query
     len = (len && len < 100) ? len : 100;
 
     var self = this,
-        auth = this.auth();
+      auth = this.auth();
 
     // TODO: loop through if `mark` is set
     // https://github.com/cpbotha/nvpy/blob/master/nvpy/simplenote.py#L211
     return auth.then(token =>
-      new Promise (
-        function (resolve, reject) {
-          request
-            .get(self.api2 + 'index')
-            .type('json')
-            .query({ auth : token, email : self.email, length : len })
-            .end(function(err, res) {
-              if(res.error) reject(res.error);
-              else if(!res.text) reject([]);
-              var json = JSON.parse(res.text);
-              self.notes = array(json.data).sort('modifydate', 'desc');
-              resolve(self.notes);
-          });
-        })
-    )
-    .catch(e => console.log(e));
+        new Promise(
+          function(resolve, reject) {
+            request
+              .get(self.api2 + 'index')
+              .type('json')
+              .query({
+                auth: token,
+                email: self.email,
+                length: len
+              })
+              .end(function(err, res) {
+                if (res.error) reject(res.error);
+                else if (!res.text) reject([]);
+                var json = JSON.parse(res.text);
+                self.notes = array(json.data).sort('modifydate', 'desc');
+                resolve(self.notes);
+              });
+          })
+      )
+      .catch(e => console.log(e));
   }
 
   get(key) {
-    if(!key) return this.all(fn);
+    if (!key) return this.all(fn);
 
     var self = this,
-        auth = this.auth();
+      auth = this.auth();
 
     return auth.then(token =>
-      new Promise (
-        function (resolve, reject) {
-          request
-            .get(self.api2 + 'data/' + key)
-            .type('json')
-            .query({ auth : token, email : self.email })
-            .end(function(err, res) {
-              if(res.error) reject(res.error);
-              else if(!res.text) reject({});
-              resolve(JSON.parse(res.text));
-          });
-        })
-    )
-    .catch(e => console.log(e));
+        new Promise(
+          function(resolve, reject) {
+            request
+              .get(self.api2 + 'data/' + key)
+              .type('json')
+              .query({
+                auth: token,
+                email: self.email
+              })
+              .end(function(err, res) {
+                if (res.error) reject(res.error);
+                else if (!res.text) reject({});
+                resolve(JSON.parse(res.text));
+              });
+          })
+      )
+      .catch(e => console.log(e));
   }
 }
 
