5d356bc86dab542c16ae769230afeaa678918d35
Kyle Hornberg <kyle.hornberg@gmail.com>
2015-07-15 10:28:35 -0500

    Remove watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,3 @@
-// Generated on 2015-07-15 using generator-nodejs 2.1.0
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -34,26 +33,14 @@ module.exports = function (grunt) {
         reporter: 'spec',
         ui: 'tdd'
       }
-    },
-    watch: {
-      js: {
-        files: ['**/*.js', '!node_modules/**/*.js'],
-        tasks: ['default'],
-        options: {
-          nospawn: true
-        }
-      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-cli');
   grunt.loadNpmTasks('grunt-babel');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.registerTask('build', ['babel:dist']);
   grunt.registerTask('test', ['jshint', 'build', 'babel:test', 'mochacli']);
-  // grunt.registerTask('watch', ['jshint', 'babel:test', 'mochacli', 'watch']);
-  grunt.registerTask('ci', ['jshint', 'babel:test', 'mochacli']);
   grunt.registerTask('default', ['test']);
 };
